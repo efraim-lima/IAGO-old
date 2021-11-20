7ffa604bd123a11d7a7b532d7914ffd916a4c664
Efraim <efraim.alima@gmail.com>
2021-11-20 17:03:47 -0300

    as copias pasteis estao funcionando melhor

--- HTML/js/index.js
+++ HTML/js/index.js
@@ -7,8 +7,28 @@ import {OrbitControls} from './three/examples/jsm/controls/OrbitControls.js'
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0xdddddd);
 
+    const camera = new THREE.PerspectiveCamera(70, window.innerWidth, window.innerHeight, 1,400);
+    camera.position.set(1,1,1);
+    camera.rotation.y = 45/180*Math.PI;
+    // camera.position.x = 1;
+    // camera.position.y = 1;
+    camera.position.z = 5;
+    scene.add(camera)
+    
+    const renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true,
+        canvas: canvas
+    })    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.shadowMap.enabled = true
+    renderer.gammaOutput = true
+    //renderer.outputEncoding = new THREE.SRGBEncoding
+
     const loader = new GLTFLoader();
-    loader.load('./assets/wraith.glb', function(gltf){
+    loader.load('./assets/skateboard.gltf', function(gltf){
         console.log(gltf);
         scene.add(gltf.scene);
         const root = gltf.scene;
@@ -20,27 +40,12 @@ import {OrbitControls} from './three/examples/jsm/controls/OrbitControls.js'
     })
 
     const dlight = new THREE.DirectionalLight(0xffffff, 1);
-    const hlight = new THREE.AmbientLight (0x404040,100);    
-    dlight.position.set(0,1,0);
-    dlight.castShadow = true;
+    const hlight = new THREE.AmbientLight (0x404040,1);    
+    // dlight.position.set(0,1,0);
+    // dlight.castShadow = true;
     scene.add(dlight);
     scene.add(hlight);
 
-    const light = new THREE.PointLight(0xc4c4c4,10);
-    light.position.set(0,300,500);
-    scene.add(light);
-
-    const light2 = new THREE.PointLight(0xc4c4c4,10);
-    light2.position.set(500,100,0);
-    scene.add(light2);
-
-    const light3 = new THREE.PointLight(0xc4c4c4,10);
-    light3.position.set(0,100,-500);
-    scene.add(light3);
-
-    const light4 = new THREE.PointLight(0xc4c4c4,10);
-    light4.position.set(-500,300,0);
-    scene.add(light4);
 
     //COMENTAR CASO O GLTF NÃO FUNCIONE::
     const geometry = new THREE.BoxGeometry(1,1,1)
@@ -48,6 +53,7 @@ import {OrbitControls} from './three/examples/jsm/controls/OrbitControls.js'
         color: 0x00ff00
     })
     const boxMesh = new THREE.Mesh(geometry, material)
+    scene.add(boxMesh)   
    
     //TERMINAR AQUI!!!
 
@@ -56,27 +62,6 @@ import {OrbitControls} from './three/examples/jsm/controls/OrbitControls.js'
         width: window.innerWidth,
         height: window.innerHeight
     }
-
-    const camera = new THREE.PerspectiveCamera(100, window.innerWidth, window.innerHeight, 0.1, 1000);
-    camera.position.set(2,2,1);
-    camera.rotation.y = 45/180*Math.PI;
-    // camera.position.x = 1;
-    // camera.position.y = 1;
-    // camera.position.z = 1;
-    scene.add(camera)
-    scene.add(boxMesh)    
-
-    const renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        canvas: canvas
-    })
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    renderer.shadowMap.enabled = true
-    renderer.gammaOutput = true
-    //renderer.outputEncoding = new THREE.SRGBEncoding
 
     const controls = new OrbitControls(camera, canvas);
     controls.update();
